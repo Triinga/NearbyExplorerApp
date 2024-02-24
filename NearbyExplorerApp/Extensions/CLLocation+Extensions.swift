cb14d17aa35b1720b56901d49902e3c2f265954e
Suhejla <suhejlahoxha03@gmail.com>
2024-02-24 01:48:28 +0100

    Map Implementation, Search Field, Table View

---
+++ NearbyExplorerApp/Extensions/CLLocation+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  CLLocation+Extensions.swift
+//  NearbyExplorerApp
+//
+//  Created by Lediona Kadiri on 23.2.24.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLLocation{
+    
+    static var `default` :CLLocation {
+        CLLocation(latitude: 36.063457, longitude: -95.880516)
+
+    }
+}
